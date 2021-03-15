7d6e8e7ca8dc08419bccfc14db7c60d00afbef56
DanielYG <72408113+DanielYG@users.noreply.github.com>
2021-03-15 23:42:29 +0100

    adapt to task3

--- W2/utilsw2.py
+++ W2/utilsw2.py
@@ -70,6 +70,44 @@ def GetGaussianModel(frames_path, number_frames ,color_space=cv2.COLOR_BGR2GRAY)
     pkl.dump(sigma, open(sigma_file, "wb"))
 
     return mu, sigma
+def remove_bg3(
+        roi,
+        Filter,
+        backSub,
+        frame_path,
+        initial_frame,
+        final_frame,
+        color_space=cv2.COLOR_BGR2GRAY, channels=(0)):
+
+    kernel = np.ones((3, 3), np.uint8)
+    c = 0
+    det_bb = []
+    for i in tqdm(range(initial_frame, final_frame)):
+        # read image
+        img = cv2.imread(frame_path + ('/frame_{:04d}.jpg'.format(i + 1)))
+        #img = cv2.cvtColor(img, color_space).astype(np.float32)
+
+        if Filter == 'yes':
+            fgMask = backSub.apply(img)
+            fgMask = fgMask & roi
+            fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_OPEN, kernel)
+            fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_CLOSE, kernel)
+            _, fgMask = cv2.threshold(fgMask, 150, 255, cv2.THRESH_BINARY)
+        else:
+            print('false')
+            fgMask = backSub.apply(img)
+        c += 1
+        det_bb += [fg_segmentation_to_boxes(fgMask, i, img)]
+
+        cv2.imshow('Frame', img)
+        cv2.imshow('FG Mask', fgMask)
+
+        keyboard = cv2.waitKey(10)
+
+        if keyboard == 'q' or keyboard == 27:
+            break
+
+    return det_bb
 
 def remove_bg(
         mu,
@@ -141,16 +179,18 @@ def denoise_bg(frame):
     filled = cv2.dilate(filled, kernel, iterations=1)
     return (filled * 255).astype(np.uint8)
 
+def bg_estimation(Method, **kwargs):
+    if Method == 'MOG2':
+        backSub = cv2.createBackgroundSubtractorMOG2()
+    elif Method == 'KNN':
+        print('KNN')
+        backSub = cv2.createBackgroundSubtractorKNN()
+    elif Method == 'LSBP':
+        backSub = cv2.bgsegm.createBackgroundSubtractorLSBP()
+    elif Method == 'MOG':
+        backSub = cv2.bgsegm.createBackgroundSubtractorMOG()
 
-def bg_estimation(mode, **kwargs):
-    if mode == 'mog':
-        return cv2.createBackgroundSubtractorMOG2(detectShadows=True)
-    if mode == 'knn':
-        return cv2.createBackgroundSubtractorKNN(detectShadows=True)
-    if mode == 'gmg':
-        return cv2.bgsegm.createBackgroundSubtractorGMG()
-    if mode == 'LSBP':
-        return cv2.bgsegm.createBackgroundSubtractorLSBP()
+    return backSub
 
 def fg_segmentation_to_boxes(frame, i,img, box_min_size=(10, 10), cls='car'):
     frame = np.ascontiguousarray(frame * 255).astype(np.uint8)
