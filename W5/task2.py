3c7def424c33561139b050d232e638f82ce3f225
Isaac <ipesanz@gmail.com>
2021-04-22 13:56:25 +0200

    Manual merge - W5_Sergi branch

--- W5/task2.py
+++ W5/task2.py
@@ -8,7 +8,8 @@ import motmetrics as mm
 import numpy as np
 from sklearn.metrics.pairwise import pairwise_distances
 
-#Class from group two of 2020.
+
+# Class from group two of 2020.
 class MOTAcumulator:
 
     def __init__(self):
@@ -36,11 +37,54 @@ class MOTAcumulator:
 
     def get_metrics(self):
         mh = mm.metrics.create()
-        summary = mh.compute(self.acc, metrics=['idf1', 'idp', 'precision', 'recall'], name='acc')
+        summary = mh.compute(self.acc, metrics=['idf1', 'idp', 'idr', 'precision', 'recall'], name='acc')
         return summary
 
-def matches(imageA, imageB, bboxA, bboxB):
 
+
+def detector(image_A, image_B, detector='sift'):
+    if detector == 'sift':
+        # Initiate SIFT detector
+        sift = cv2.SIFT_create()
+        print(detector)
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(image_A, None)
+        kp2, des2 = sift.detectAndCompute(image_B, None)
+        # BFMatcher with default params
+        bf = cv2.BFMatcher()
+
+        matches = bf.knnMatch(des1, des2, k=2)
+
+    elif detector == 'orb':
+        # Initiate STAR detector
+        orb = cv2.ORB_create()
+        print(detector)
+
+        # compute the descriptors with ORB
+        kp1, des1 = orb.detectAndCompute(image_A, None)
+        kp2, des2 = orb.detectAndCompute(image_B, None)
+        # BFMatcher with default params
+        bf = cv2.BFMatcher()
+
+        matches = bf.knnMatch(des1, des2, k=2)
+
+    elif detector == 'surf':
+        # Initiate STAR detector
+        surf = cv2.xfeatures2d.SURF_create(400)
+        print(detector)
+
+        # compute the descriptors with ORB
+        kp1, des1 = surf.detectAndCompute(image_A, None)
+        kp2, des2 = surf.detectAndCompute(image_B, None)
+        # BFMatcher with default params
+        bf = cv2.BFMatcher()
+
+        matches = bf.knnMatch(des1, des2, k=2)
+
+    return matches
+
+
+def matches(imageA, imageB, bboxA, bboxB):
     '''
     try:
         cv2.imshow('tracking detections 1', cv2.resize(imageA, (900, 600)))
@@ -64,46 +108,37 @@ def matches(imageA, imageB, bboxA, bboxB):
         imageB = imageB[(int(bboxB.bbox[1])):(int(bboxB.bbox[3])),
                  (int(bboxB.bbox[0])):(int(bboxB.bbox[2]))]
         imageA = cv2.resize(imageA, (100, 100))
-        imageAc = cv2.cvtColor(imageA, cv2.COLOR_BGR2HSV)
+        # imageA = cv2.cvtColor(imageA, cv2.COLOR_BGR2YCrCb)
         imageB = cv2.resize(imageB, (100, 100))
-        imageBc = cv2.cvtColor(imageB, cv2.COLOR_BGR2HSV)
+        # imageB = cv2.cvtColor(imageB, cv2.COLOR_BGR2YCrCb)
 
-        cv2.imshow('tracking detections 1', cv2.resize(imageA, (900, 600)))
+        '''cv2.imshow('tracking detections 1', cv2.resize(imageA, (900, 600)))
         cv2.imshow('tracking detections 2', cv2.resize(imageB, (900, 600)))
-        cv2.waitKey(0)
+        cv2.waitKey(0)'''
 
-        # Initiate SIFT detector
+        matches = detector(imageA, imageB, detector='sift')
+        '''# Initiate SIFT detector
         sift = cv2.SIFT_create()
         # find the keypoints and descriptors with SIFT
         kp1, des1 = sift.detectAndCompute(imageA, None)
         kp2, des2 = sift.detectAndCompute(imageB, None)
         # BFMatcher with default params
-
         bf = cv2.BFMatcher()
 
-        matches = bf.knnMatch(des1, des2, k=2)
+        matches = bf.knnMatch(des1, des2, k=2)'''
         # Apply ratio test
         good = []
         for m, n in matches:
             if m.distance < 0.9 * n.distance:
                 good.append([m])
-
-        h1 = cv2.calcHist([imageAc], [1], None, [256], [0, 256])
-        h1 = cv2.normalize(h1, h1, 0, 1, cv2.NORM_MINMAX, -1)
-
-        h2 = cv2.calcHist([imageBc], [1], None, [256], [0, 256])
-        h2 = cv2.normalize(h2, h2, 0, 1, cv2.NORM_MINMAX, -1)
-
-        colour = cv2.compareHist(h1, h2, 0)
-
     except:
         good = []
-        colour = 0
 
-    return good, colour
+    return good
 
 
-def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = False, wz = 0, model='yolo3', seq = 'S03'):
+def task2(path_to_frames2, save_frames=False, th=1, mask=[0, 0], cam=['c010', 'c011', 'c012'], op=False, wz=0,
+          model='yolo3', seq='S03'):
     # Reading inputs.
     # If you need to save the frames --> save_frames = True. False == reading from path
     if save_frames:
@@ -121,7 +156,8 @@ def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = 
 
     video_n_frames = len(glob.glob1(path_to_frames, "*.jpg"))
     # Reading the groundtruth and getting the Bounding Boxes per frame cam1
-    reader1 = AICityChallengeAnnotationReader(path='datasets/aic19-track1-mtmc-train/train/{}/{}/gt/gt.txt'.format(seq, cam[0]))
+    reader1 = AICityChallengeAnnotationReader(
+        path='../datasets/aic19-track1-mtmc-train/train/{}/{}/gt/gt.txt'.format(seq, cam[0]))
     gt_file1 = reader1.get_annotations(classes=['car'])
     gt_bb1 = []
     for frame in range(int(500), int(video_n_frames)):
@@ -129,7 +165,8 @@ def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = 
         gt_bb1.append(annotations)
 
     # Reading the groundtruth and getting the Bounding Boxes per frame cam1
-    reader2 = AICityChallengeAnnotationReader(path='datasets/aic19-track1-mtmc-train/train/{}/{}/gt/gt.txt'.format(seq, cam[1]))
+    reader2 = AICityChallengeAnnotationReader(
+        path='../datasets/aic19-track1-mtmc-train/train/{}/{}/gt/gt.txt'.format(seq, cam[1]))
     gt_file2 = reader1.get_annotations(classes=['car'])
     gt_bb2 = []
     for frame in range(int(500), int(video_n_frames)):
@@ -137,11 +174,13 @@ def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = 
         gt_bb2.append(annotations)
 
     # Reading the detections cam1
-    reader1 = AICityChallengeAnnotationReader(path='datasets/aic19-track1-mtmc-train/train/{}/{}/mtsc/mtsc_tc_{}.txt'.format(seq, cam[0], model))
+    reader1 = AICityChallengeAnnotationReader(
+        path='../datasets/aic19-track1-mtmc-train/train/{}/{}/mtsc/mtsc_tc_{}.txt'.format(seq, cam[0], model))
     det_file1 = reader1.get_annotations(classes=['car'])
 
     # Reading the detections cam2
-    reader2 = AICityChallengeAnnotationReader(path='datasets/aic19-track1-mtmc-train/train/{}/{}/mtsc/mtsc_tc_{}.txt'.format(seq, cam[1], model))
+    reader2 = AICityChallengeAnnotationReader(
+        path='../datasets/aic19-track1-mtmc-train/train/{}/{}/mtsc/mtsc_tc_{}.txt'.format(seq, cam[1], model))
     det_file2 = reader2.get_annotations(classes=['car'])
 
     bb_frames1, bb_frames2 = [], []
@@ -152,16 +191,21 @@ def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = 
     # Create an accumulator that will be updated during each frame
     acc1 = MOTAcumulator()
     acc2 = MOTAcumulator()
-    track1_tot, track2_tot = [],[]
+    track1_tot, track2_tot = [], []
 
-    a = int(video_n_frames * 0.25)
-    for frame in range(int(500), int(video_n_frames)):
+    rng = [len(os.listdir(path_to_frames)), len(os.listdir(path_to_frames2))]
+
+    a = min(rng)
+    i = 0
+    for frame in range(int(0), a):
 
         det1 = det_file1.get(frame, [])
-        img1 = cv2.imread(path_to_frames + ('/frame_{:04d}.jpg'.format(frame + 1)))
+        path1 = path_to_frames + ('/frame_{:04d}.jpg'.format(frame + 1))
+        img1 = cv2.imread(path1)
 
         det2 = det_file2.get(frame, [])
-        img2 = cv2.imread(path_to_frames2 + ('/frame_{:04d}.jpg'.format(frame + 1)))
+        path2 = path_to_frames2 + ('/frame_{:04d}.jpg'.format(frame + 1))
+        img2 = cv2.imread(path2)
 
         frame_tracks1, frame_tracks2 = [], []
         for track in tracks1:
@@ -185,9 +229,9 @@ def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = 
                                 track1_tot[i] = [t[0], matched_det1, img1]
                         max_good = []
                         for t in track2_tot:
-                            good, colour = matches(img1, t[2], matched_det1, t[1])
-                            print(len(good)*abs(colour))
-                            if len(good)*abs(colour) > len(max_good):
+                            good = matches(img1, t[2], matched_det1, t[1])
+                            print(len(good))
+                            if len(good) > len(max_good):
                                 max_good = good
                                 save_t = t
                         if len(max_good) > 25:
@@ -199,7 +243,6 @@ def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = 
                             for i, t in enumerate(track1_tot):
                                 if t[0] == remove_id:
                                     track1_tot.pop(i)
-
                     if track.buffer > 4:
                         if id_remove1 == False:
                             track.add_detection(matched_det1)
@@ -218,9 +261,9 @@ def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = 
 
         # New track detection
         for new_bb in det1:
-            if (new_bb.bbox[3] > mask[0] and new_bb.bbox[2] > mask[1])\
-                    and (new_bb.bbox[3]-new_bb.bbox[1]) > wz[0] \
-                    and (new_bb.bbox[3]-new_bb.bbox[1]) < wz[1] \
+            if (new_bb.bbox[3] > mask[0] and new_bb.bbox[2] > mask[1]) \
+                    and (new_bb.bbox[3] - new_bb.bbox[1]) > wz[0] \
+                    and (new_bb.bbox[3] - new_bb.bbox[1]) < wz[1] \
                     and (new_bb.bbox[2] - new_bb.bbox[0]) > wz[0] \
                     and (new_bb.bbox[2] - new_bb.bbox[0]) < wz[1]:
                 new_bb.id = max_track + 1
@@ -251,9 +294,9 @@ def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = 
                                 track2_tot[i] = [t[0], matched_det2, img2]
                         max_good = []
                         for t in track1_tot:
-                            good, colour = matches(t[2], img2 , t[1], matched_det2)
-                            print(len(good)*abs(colour))
-                            if len(good)*abs(colour) > len(max_good):
+                            good = matches(t[2], img2, t[1], matched_det2)
+                            print(len(good))
+                            if len(good) > len(max_good):
                                 max_good = good
                                 save_t = t
                         if len(max_good) > 25:
@@ -283,9 +326,9 @@ def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = 
 
         # New track detection
         for new_bb in det2:
-            if (new_bb.bbox[3] > mask[2] and new_bb.bbox[2] > mask[3])\
-                    and (new_bb.bbox[3]-new_bb.bbox[1]) > wz[2] \
-                    and (new_bb.bbox[3]-new_bb.bbox[1]) < wz[3] \
+            if (new_bb.bbox[3] > mask[2] and new_bb.bbox[2] > mask[3]) \
+                    and (new_bb.bbox[3] - new_bb.bbox[1]) > wz[2] \
+                    and (new_bb.bbox[3] - new_bb.bbox[1]) < wz[3] \
                     and (new_bb.bbox[2] - new_bb.bbox[0]) > wz[2] \
                     and (new_bb.bbox[2] - new_bb.bbox[0]) < wz[3]:
                 new_bb.id = max_track + 1
@@ -323,32 +366,52 @@ def task2(save_frames=False, th = 1, mask = [0, 0],cam = ['c010', 'c011'], op = 
         acc2.update(moc_gt2[-1], moc_pred2[-1])
 
         if True:
-            cv2.imshow('tracking detections 1', cv2.resize(img1, (900, 600)))
-            cv2.imshow('tracking detections 2', cv2.resize(img2, (900, 600)))
+            a = cv2.resize(img1, (700, 400))
+            b = cv2.resize(img2, (700, 400))
+            # cv2.imshow('tracking detections 1', cv2.resize(img1, (700, 400)))
+            # cv2.imshow('tracking detections 2', cv2.resize(img2, (700, 400)))
+
+            total = np.concatenate((a, b), axis=1)
+            cv2.imwrite(f'../W5/outs/S04/out_cam_c39_frame{i}.jpg', total)
+            i+=1
+            #cv2.imshow('Tracking', cv2.resize(total, (1000, 500)))
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
         bb_frames1.append(frame_det1)
 
+
     iou = compute_iou_over_time(gt_bb1, bb_frames1)
 
     ap, prec, rec = mean_average_precision(gt_bb1, bb_frames1)
 
+    metrics = acc1.get_metrics()
+    print('IDF1: ', metrics.idf1['acc'])
+    print('IDP: ', metrics.idp['acc'])
+    print('IDR: ', metrics.idr['acc'])
+    print('Precision: ', metrics.precision['acc'])
+    print('Recall: ', metrics.recall['acc'])
+
     print('{} AP {}: '.format(model, cam), ap)
     print('{} IoU {}: '.format(model, cam), iou[0])
-    print('\nAdditional metrics IDF1 {}:'.format(cam))
-    print(acc1.get_idf1())
+    '''print('\nAdditional metrics IDF1 {}:'.format(cam))
+    print(acc1.get_idf1())'''
+
 
 if __name__ == '__main__':
 
-        th = [0.96]
-        model = 'yolo3'
-        cam = ['c010', 'c011']
-        seq = 'S03'
+    th = [0.96]
+    model = 'yolo3'
+    # cam = ['c014', 'c015']
+    cam = ['c039', 'c040']
+    #cam = ['c001', 'c002']
+    #seq = 'S01'
+    #seq = 'S03'
+    seq = 'S04'
 
-        '''
+    '''
         'c010':
-            mask = [150, 500]
+            mask = [150, 0]
             wz = [75, 1500]
         'c011':
             mask = [800, 1300]
@@ -365,15 +428,18 @@ if __name__ == '__main__':
         'c015':
             mask = [850, 500]
         '''
-        #cam1 --> mask[0],mask[1]
-        #cam2 --> mask[2], mask[3]
-        #same wz
-        mask = [100, 500, 800, 1300]
-        wz = [75, 1500, 100, 1500]
+    # cam1 --> mask[0],mask[1]
+    # cam2 --> mask[2], mask[3]
+    # same wz
+    # mask = [1920, 700]
+    # Si seq =! S03 mask = [0,0,0,0]
+    mask = [150, 500, 800, 1300]
+    wz = [75, 1500, 100, 1500]
 
-        path_to_video = 'datasets/aic19-track1-mtmc-train/train/{}/{}/vdo.avi'.format(seq, cam[0])
-        path_to_frames = 'datasets/{}/'.format(cam[0])
-        path_to_frames2 = 'datasets/{}/'.format(cam[1])
+    path_to_video = '../datasets/aic19-track1-mtmc-train/train/{}/{}/vdo.avi'.format(seq, cam[0])
+    path_to_frames = '../datasets/{}/'.format(cam[0])
+    path_to_frames2 = '../datasets/{}/'.format(cam[1])
 
-        for t in th:
-            task2(save_frames=False, th=t, mask=mask, op=False, cam=cam, wz=wz, model=model, seq=seq)
+    for t in th:
+        task2(path_to_frames2=path_to_frames2, save_frames=False, th=t, mask=[0, 0, 0, 0], op=False, cam=cam, wz=wz,
+              model=model, seq=seq)
